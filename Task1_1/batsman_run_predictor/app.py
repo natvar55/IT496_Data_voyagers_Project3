dc1785b7800ed4c26360570e9e24a17b9baba3b9
Natvar Prajapati <97463955+natvar55@users.noreply.github.com>
2023-11-14 21:29:21 +0530

    Update app.py

--- Task1_1/batsman_run_predictor/app.py
+++ Task1_1/batsman_run_predictor/app.py
@@ -96,11 +96,11 @@ if st.button('Predict Score'):
     df = pd.DataFrame({f'player_{name}': [0] for name in player})
     df1 = pd.DataFrame({f'opposition_{name}': [0] for name in opposition})
     df1 = pd.DataFrame({f'ground_{name}': [0] for name in ground})
-    str1='player_'+[player]
+    str1 = f'player_{player}'
     df[str1]=1
-    str1='opposition_'+[opposition]
+    str1 = f'opposition_{opposition}'
     df[str1]=1
-    str1='ground_'+[ground]
+    str1 = f'ground_{ground}'
     df[str1]=1
     df['avg_4s']=avg_4s
     df['avg_6s']=avg_6s
